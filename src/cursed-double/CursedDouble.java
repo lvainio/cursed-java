e8c502f123b6299a35f71f1caf4bf1b5ab417156
lvainio <lvainio@kth.se>
2025-01-22 10:20:18 +0100

    Modify cursed double

--- src/cursed-double/CursedDouble.java
+++ src/cursed-double/CursedDouble.java
@@ -4,6 +4,9 @@ public class CursedDouble {
         if (!(d1 == d1)) {
             System.out.println("d1 is not equal to itself!");
         }
+        if (Double.valueOf(d1).equals(Double.valueOf(d1))) {
+            System.out.println("d1 is equal to itself!");
+        }
 
         double d2 = 1.0 / 49.0;
         if (1.0 != d2 * 49.0) {
