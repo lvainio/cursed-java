c26ebfca19712094b16c73e291cc50ed54604ed3
lvainio <lvainio@kth.se>
2025-02-21 16:20:22 +0100

    Add

---
+++ src/cursed-abs/CursedAbs.java
@@ -0,0 +1,5 @@
+public class CursedAbs {
+    public static void main(String[] args) {
+        System.out.println("The absolute value of -2147483648 is: " + Math.abs(-2147483648));
+    }
+}
--- src/cursed-while/CursedWhile.java
+++ src/cursed-while/CursedWhile.java
@@ -1,6 +1,7 @@
 public class CursedWhile {
     public static void main(String[] args) {
         int x = 10;
+        https://www.google.com
         while (x  -->  0) { 
             System.out.println(x);
         }
