d90e6530e8deb10d2bc1067d105c504046715752
lvainio <lvainio@kth.se>
2025-02-21 10:54:09 +0100

    Add comment

---
+++ src/cursed-comment/CursedComment.java
@@ -0,0 +1,11 @@
+public class CursedComment {
+    public static void main(String... args) {
+        //\u000A sayHello();
+    }
+
+    /*\u002A\u002F  
+        private static void sayHello() {
+            System.out.println("Hello, World!");
+        }
+    \u002F\u002A*/
+}
